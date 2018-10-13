452d9522eeeb21bd72fbb8f19ebf719287ae4204
Lorefnon <lorefnon@gmail.com>
2018-10-13 12:48:47 +0530

    test: Add tests for happy paths

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,26 +1,47 @@
-import * as mock from "mock-fs"
-import * as fs from "fs"
+import * as fs from "fs-extra"
 import * as path from "path"
+import * as tmp from "tmp"
 import { run } from "."
 
-const readFixtureSync = (filePath: string) =>
-  fs.readFileSync(path.join(__dirname, "__fixtures__", filePath), {
-    encoding: "utf8"
+const populateFixtures = async (projDir: string, filePaths: string[]) => {
+  for (const filePath of filePaths) {
+    const srcPath = path.join(__dirname, "__fixtures__", filePath)
+    const destPath = path.join(projDir, filePath)
+    await fs.ensureDir(path.dirname(destPath))
+    const content = await fs.readFile(srcPath, { encoding: "utf8" })
+    await fs.writeFile(destPath, content)
+  }
+}
+
+const getTmpDir = () =>
+  new Promise<string>((resolve, reject) => {
+    tmp.dir({ keep: true }, (err, dir) => {
+      if (err) reject(err)
+      else resolve(dir)
+    })
   })
 
 describe("TemplateProcessor", () => {
+  let projDir: string | null = null
+  let originalCwd: string | null = null
+  beforeEach(() => {
+    originalCwd = process.cwd()
+  })
+  afterEach(async () => {
+    process.chdir(originalCwd!)
+    if (projDir) {
+      fs.remove(projDir)
+      projDir = null
+    }
+  })
   it("Injects generated content into annotated blocks", async () => {
-    mock({
-      "ingenr-generators": {
-        "knex-dal.dot": readFixtureSync("ingenr-generators/knex-dal.dot")
-      },
-      src: {
-        "index.ts": readFixtureSync("src/index.ts")
-      }
-    })
+    projDir = await getTmpDir()
+    await populateFixtures(projDir!, ["src/index.ts", "ingenr-generators/knex-dal.dot"])
+    process.chdir(projDir!)
     await run()
-    const data = fs.readFileSync("src/index.ts", { encoding: "utf8" })
-    mock.restore()
-    console.log("data =>", data)
+    const postProcessedContents = await fs.readFile(path.join(projDir!, "src/index.ts"), {
+      encoding: "utf8"
+    })
+    expect(postProcessedContents).toMatchSnapshot()
   })
 })
